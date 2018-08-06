992fdb8ce6a5d460943fcfc2edc1244d62e6437a
sbx_user1060 <sbx_user1060@ip-10-12-36-87.eu-west-2.compute.internal>
2018-08-06 21:38:29 +0000

    Mon Aug 06 21:38:29 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -7,25 +7,17 @@ public class FizzBuzzSolution {
     public String fizzBuzz(Integer number) {
         String answer = "";
         String stringNumber = String.valueOf(number);
-        boolean number3Condition = number % 3 == 0  || stringNumber.contains("3");
-        boolean number5Condition = number %5 == 0 || stringNumber.contains("5");
-        boolean containsSameDigits = true;
-        for(int i=1; i < stringNumber.length(); i++) {
-            if(stringNumber.charAt(i) != stringNumber.charAt(i-1)) {
-                containsSameDigits = false;
-                break;
-            }
-        }
-        boolean deluxeCondition = number > 10 && containsSameDigits;
-        if( number3Condition) {
+        boolean fizzCondition = number % 3 == 0  || stringNumber.contains("3");
+        boolean buzzCondition = number %5 == 0 || stringNumber.contains("5");
+        boolean deluxeCondition = (number % 3 == 0  && stringNumber.contains("3")) || (number %5 == 0 && stringNumber.contains("5"));
+        String deluxeValue = number%2 == 0 ? "deluxe" : "fake deluxe";
+
+        if(fizzCondition) {
             answer = "fizz";
-        } if(number5Condition) {
-
-            answer = answer.concat(number3Condition ? " buzz" : "buzz");
-
+        } if(buzzCondition) {
+            answer = answer.concat(fizzCondition ? " buzz" : "buzz");
         } if(deluxeCondition) {
-            String deluxeValue = number%2 != 0 ? "fake deluxe" : "deluxe";
-            answer = answer.concat(number3Condition || number5Condition ? " " : "");
+            answer = answer.concat(fizzCondition || buzzCondition ? " " : "");
             answer = answer.concat(deluxeValue);
         }
         if(answer.equals("")) {
