0ad52509b8fab35b2f196f60cbe73ca5c39eaf82
sbx_user1060 <sbx_user1060@ip-10-12-36-87.eu-west-2.compute.internal>
2018-08-06 21:13:15 +0000

    Mon Aug 06 21:13:15 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -5,17 +5,25 @@ import befaster.runner.SolutionNotImplementedException;
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-        String answer;
+        String answer = "";
         String stringNumber = String.valueOf(number);
         boolean number3Condition = number % 3 == 0  || stringNumber.contains("3");
         boolean number5Condition = number %5 == 0 || stringNumber.contains("5");
-        if( number3Condition && number5Condition ) {
-            answer = "fizz buzz";
-        } else if(number5Condition) {
-            answer = "buzz";
-
-        } else if(number3Condition) {
+        boolean containsSameDigits = true;
+        for(int i=1; i < stringNumber.length(); i++) {
+            if(stringNumber.charAt(i) != stringNumber.charAt(i-1)) {
+                containsSameDigits = false;
+                break;
+            }
+        }
+        boolean deluxeCondition = number > 10 && containsSameDigits;
+        if( number3Condition) {
             answer = "fizz";
+        } else if(number5Condition) {
+            answer += number3Condition ? " buzz" : "buzz";
+
+        } else if(deluxeCondition) {
+            answer += number5Condition ? " deluxe" : "deluxe";
         } else {
             answer = String.valueOf(number);
         }
